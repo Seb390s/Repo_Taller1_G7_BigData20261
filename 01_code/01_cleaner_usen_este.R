a29b685182f445d594e1ed9797264f11aa2d8e32
Sebastian-Rodriguez--Cabrera <s.rodriguezc2@uniandes.edu.co>
2026-02-19 18:34:53 -0500

    Update 01_cleaner_usen_este.R

--- 01_code/01_cleaner_usen_este.R
+++ 01_code/01_cleaner_usen_este.R
@@ -4,12 +4,13 @@
 
 base <- GEIH2018 %>% filter(age>18, ocu ==1) 
 
+#AGREGAR FILTRO DE SALARIO POSITIVO (PONER LA VARIABLE SEBAS)
+
 base <- base %>% mutate (
   logw=log(y_total_m),
   agecua = age^2
 )
 
-#AGREGAR FILTRO DE SALARIO POSITIVO (PONER LA VARIABLE SEBAS)
 
 #tABLA VARIABLE, TIPO DE VARIABLE, #MISSINGS Y #PORCENTAJE DE MISSING
 #(Esto sale de limpiezaDatosrevisar y datacleaner revisar)
